47df603b101f5dbe73b6b8a671f4fba349013ec7
sfux <samuel.fux@id.ethz.ch>
2022-07-26 10:46:30 +0000

    Update start_jupyter_nb.sh

--- start_jupyter_nb.sh
+++ start_jupyter_nb.sh
@@ -151,7 +151,7 @@ Examples:
 
         ./start_jupyter_nb.sh -u sfux -n 4 -W 04:00 -m 2048 -w /cluster/scratch/sfux
 
-        ./start_jupyter_nb.sh -u sfxu -n 1 -W 01:00 -m 1024 -j TRUE
+        ./start_jupyter_nb.sh -u sfux -n 1 -W 01:00 -m 1024 -j TRUE
 
         ./start_jupyter_nb.sh --username sfux --numcores 2 --runtime 01:30 --memory 2048 --softwarestack new
 
